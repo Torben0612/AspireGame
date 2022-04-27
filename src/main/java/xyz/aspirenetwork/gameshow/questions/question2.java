5748564ac292cfcf64787cf73b1fbc1bf1588fbd
Torben0612 <74421064+Torben0612@users.noreply.github.com>
2022-04-27 08:26:24 -0400

    Create question2.java

---
+++ src/main/java/xyz/aspirenetwork/gameshow/questions/question2.java
@@ -0,0 +1,36 @@
+package xyz.aspirenetwork.gameshow.questions;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BookMeta;
+
+public class Question1 implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        Player player = (Player) sender;
+
+        if (sender instanceof Player){
+
+            ItemStack q1 = new ItemStack(Material.WRITTEN_BOOK);
+            BookMeta meta = (BookMeta) q1.getItemMeta();
+
+            meta.setTitle(ChatColor.WHITE + "QUESTION 1");
+            meta.setAuthor(ChatColor.BLUE + "Aspire Game Show");
+            meta.addPage(
+                    ChatColor.BLUE + "Question 1:" +
+                            "\n" + ChatColor.BLUE + "INSERT-QUESTION-2-HERE"
+            );
+            q1.setItemMeta(meta);
+            for(Player player : Bukkit.getOnlinePlayers){
+                
+                player.getInventory().addItem(q1);
+            }
+        }
+        return false;
+    }
+}
